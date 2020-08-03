47a0a53286b8ed86de15831f75a638bc2e6b7dfd
seogy <seogy20@gmail.com>
2020-08-03 14:41:38 +0900

    Nuxt와 Vuetify 도입하기

---
+++ ch1/front/components/FollowList.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-list-tile>
+    <li>
+      <span>제로초</span>
+      <v-icon>mdi-minus-circle-outline</v-icon>
+    </li>
+    <li>
+      <span>네로</span>
+      <v-icon>mdi-minus-circle-outline</v-icon>
+    </li>
+    <li>
+      <span>히어로</span>
+      <v-icon>mdi-minus-circle-outline</v-icon>
+    </li>
+  </v-list-tile>
+
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+<style>
+
+</style>
