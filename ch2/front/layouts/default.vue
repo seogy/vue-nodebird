bde2fa44ec92f0475cc9e1ce451a45282c8a517f
seogy <seogy20@gmail.com>
2020-08-04 13:18:35 +0900

    ch1을 ch2로 복사

---
+++ ch2/front/layouts/default.vue
@@ -0,0 +1,44 @@
+<template>
+  <v-app>
+    <nav>
+      <v-toolbar dark color="green">
+        <v-toolbar-title>
+          <nuxt-link to="/">NodeBird</nuxt-link>
+        </v-toolbar-title>
+        <v-spacer></v-spacer>
+        <v-toolbar-items>
+          <v-text-field label="검색" hide-details prepend-icon="mdi-magnify" :style="{ display: 'flex', alignItems: 'center' }" ></v-text-field>
+          <v-btn text nuxt to="/profile">
+            <div>프로필</div>
+          </v-btn>
+          <v-btn text nuxt to="/signup">
+            <div>회원가입</div>
+          </v-btn>
+        </v-toolbar-items>
+      </v-toolbar>
+      <v-row no-gutters>
+        <v-col cols="12" md="4">
+          <LoginForm/>
+        </v-col>
+        <v-col cols="12" md="8">
+          <nuxt />
+        </v-col>
+      </v-row>
+    </nav>
+  </v-app>
+</template>
+
+<script>
+import LoginForm from '~/components/LoginForm';
+
+
+  export default {
+    components: {
+      LoginForm
+    }
+  }
+</script>
+
+<style>
+
+</style>
---
+++ ch2/front/nuxt.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  head() {
+    return {
+      title: 'NodeBird',
+    }
+  },
+  buildModules: [
+    '@nuxtjs/vuetify'
+  ],
+  vuetify: {
+    /* module options */
+  },
+  plugins: [],
+  css: ["@mdi/font/css/materialdesignicons.css"]
+};
